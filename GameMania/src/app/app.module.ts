fe125c1c4a0f7d6f14d9334deff07bfb7c9cfb88
Henrique <henriquecorrea.nem@outlook.com>
2022-11-23 09:46:07 -0300

    primeiro commit

---
+++ GameMania/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ GameMania/src/assets/js/script.js
@@ -0,0 +1,30 @@
+
+
+function mostrarMenu() {
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value
+     
+    alert(email)
+    console.log(email)
+
+}
+
+function logar() {
+    let login = document.getElementById("email").value
+
+    alert(login)
+    console.log(login)
+
+}
+
+    
